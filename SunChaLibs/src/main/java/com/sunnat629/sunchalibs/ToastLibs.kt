48c363888d37dcdd335ff598f8a93b90f68e3fd6
S M Mohi Us Sunnat <suncha629@gmail.com>
2019-01-23 21:30:26 +0600

    creating new repo

---
+++ SunChaLibs/src/main/java/com/sunnat629/sunchalibs/ToastLibs.kt
@@ -0,0 +1,48 @@
+package com.sunnat629.sunchalibs
+
+import android.content.Context
+import android.view.Gravity
+import android.widget.Toast
+
+class ToastLibs {
+    private lateinit var toast: Toast
+
+    fun toast(context: Context, message: String){
+        Toast.makeText(context.applicationContext, message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun toast(context: Context, message: String, duration: Int){
+        Toast.makeText(context.applicationContext, message, duration).show()
+    }
+
+
+    fun toastCenter(context: Context, message: String){
+        toast = Toast.makeText(context.applicationContext, message, Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0)
+        toast.show()
+    }
+
+    fun toastTop(context: Context, message: String){
+        toast = Toast.makeText(context.applicationContext, message, Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.TOP, 0, 0)
+        toast.show()
+    }
+
+    fun toastBottom(context: Context, message: String){
+        toast = Toast.makeText(context.applicationContext, message, Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.BOTTOM, 0, 0)
+        toast.show()
+    }
+
+    fun toastLeft(context: Context, message: String){
+        toast = Toast.makeText(context.applicationContext, message, Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.START, 0, 0)
+        toast.show()
+    }
+
+    fun toastRight(context: Context, message: String){
+        toast = Toast.makeText(context.applicationContext, message, Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.END, 0, 0)
+        toast.show()
+    }
+}
---
+++ app/src/main/java/com/sunnat629/suncha/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.sunnat629.suncha
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
